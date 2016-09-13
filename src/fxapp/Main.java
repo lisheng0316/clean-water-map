3d7f7d46b207a282e88771056d682ff25a2b5aeb
Sheng Li <sli471@gatech.edu>
2016-09-13 16:43:03 -0400

    initial commit for M4

--- src/fxapp/Main.java
+++ src/fxapp/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package fxapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
---
+++ src/model/User.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by Sheng on 9/13/16.
+ */
+public class User {
+}
