5f7e87bce6010c0d7930ce3a90d3301e0e34b529
Sheng Li <sli471@gatech.edu>
2016-09-20 16:12:08 -0400

    initialize User class

--- src/model/User.java
+++ src/model/User.java
@@ -1,7 +1,20 @@
 package model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  * Created by Sheng on 9/13/16.
  */
 public class User {
+    private final StringProperty _name = new SimpleStringProperty();
+    private final StringProperty _password = new SimpleStringProperty();
+
+    public String getName() { return _name.get(); }
+    public void setName(String name) { _name.set(name); }
+
+    public String getPassword() {return _password.get(); }
+    public void setPassword(String password) { _password.set(password); }
+
+
 }
