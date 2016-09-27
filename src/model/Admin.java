fd94ef72b816c9a68581e79e3b2bd5e97ec093de
Sheng Li <sli471@gatech.edu>
2016-09-27 17:57:55 -0400

    Change the User to Account for registration purpose. Create Primary Actors classes

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -3,6 +3,9 @@ package model;
 /**
  * Created by Sheng on 9/27/16.
  */
-public class Admin {
+public class Admin extends User{
+    public Admin(String id, String fname, String lname, String email, String rank) {
+        super(id, fname, lname, email, rank);
+    }
 
 }
